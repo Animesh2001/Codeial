8f8b7019e4bc629567429a7fd810e1c78d8cdde0
Animesh Jaiswal <animeshj786@gmail.com>
2022-05-13 23:45:07 +0530

    Display Posts and related User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,13 +1,28 @@
 //const { render } = require("ejs")
+const Post = require('../models/post')
 
 module.exports.home=function(req,res){
-       // return res.end('<h1>Express is up for Codeial</h1>');
-       
-//        console.log(req.cookies);
-        res.cookie('user_id',170)
-       return res.render('home',{
-               title:"Home"
-       });
+        //  return res.end('<h1>Express is up for Codeial</h1>');
+        //  console.log(req.cookies);
+        //  res.cookie('user_id',170)
+      
+
+        //this post.find({}) query will return all the post from the database
+        // Post.find({},function(err,posts){
+        //         return res.render('home',{
+        //                 title:"Codeial | Home",
+        //                 posts:posts
+        //         });
+        // });
+
+
+        //populate the user of each post
+        Post.find({}).populate('user').exec(function(err,posts){
+                return res.render('home',{
+                        title:"Codeial | Home",
+                        posts:posts
+                });
+        });
 }
 
 //module.exports.actionName=function(req,res){}
