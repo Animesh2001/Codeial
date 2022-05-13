bf98e56189370caac4e71e19e3ed9e1b5cecc7ce
Animesh Jaiswal <animeshj786@gmail.com>
2022-05-13 22:41:58 +0530

    Savings Post to the DB

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,8 @@ router.use('/users',require('./users'));
 
 router.use('/practice',require('./practice'));
 
+router.use('/posts',require('./posts'));    
+
 //for any further routes, access from here
 //router.use('/routerName',require('./routerFile'));
 
