ec31cc67d6ee3da7422bf7608403dde99cfb83bb
Animesh Jaiswal <animeshj786@gmail.com>
2022-05-14 13:15:42 +0530

    Check authentication on creating post

--- controllers/posts_controllers.js
+++ controllers/posts_controllers.js
@@ -3,6 +3,7 @@ const Post=require('../models/post')
 module.exports.create=function(req,res){
     Post.create({
         content:req.body.content,
+        //doubt
         user:req.user._id
     },function(err,post){
         if(err){console.log('error in creating a post'); return;}
