f3c34822cefba6cca04969d45f67573b948b5a8b
Animesh Jaiswal <animeshj786@gmail.com>
2022-05-20 23:18:52 +0530

    Delete a Post (Authorized)

--- controllers/posts_controllers.js
+++ controllers/posts_controllers.js
@@ -1,4 +1,5 @@
-const Post=require('../models/post')
+const Post=require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create=function(req,res){
     Post.create({
@@ -11,3 +12,18 @@ module.exports.create=function(req,res){
     });
 }
 
+module.exports.destroy=function(req,res){
+    //first we check if the post exist
+    Post.findById(req.params.id,function(err,post){
+    //.id automatically converts the object id to String
+    if(post.user==req.user.id){
+        post.remove();
+    //we just done here err check as we don't want further the deleted comments in DB
+        Comment.deleteMany({post:req.params.id},function(err){
+            return res.redirect('back');
+        });
+    }else{
+        return res.redirect('back');
+    }
+  });
+}
