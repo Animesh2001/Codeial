eec194c072f2f2771c0a868d485ec5960e9f121d
Animesh Jaiswal <animeshj786@gmail.com>
2022-05-20 18:55:38 +0530

    Adding Comments to the DB

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,8 @@ router.use('/practice',require('./practice'));
 
 router.use('/posts',require('./posts'));    
 
+router.use('/comments',require('./comments'));
+
 //for any further routes, access from here
 //router.use('/routerName',require('./routerFile'));
 
