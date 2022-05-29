c91db9a3bd7e0ec6782da908d0cd8b8d72593cf0
Animesh Jaiswal <animeshj786@gmail.com>
2022-05-29 16:28:27 +0530

    Converting more code to Async Await

--- controllers/posts_controllers.js
+++ controllers/posts_controllers.js
@@ -1,29 +1,36 @@
 const Post=require('../models/post');
 const Comment = require('../models/comment');
 
-module.exports.create=function(req,res){
-    Post.create({
-        content:req.body.content,
-        //doubt
-        user:req.user._id
-    },function(err,post){
-        if(err){console.log('error in creating a post'); return;}
+module.exports.create= async function(req,res){
+    try{
+        Post.create({
+            content:req.body.content,
+            //doubt
+            user:req.user._id
+        });
         return res.redirect('back');
-    });
+    }catch(err){
+        console.log('Error',err);
+        return;
+    }
+    
 }
 
-module.exports.destroy=function(req,res){
-    //first we check if the post exist
-    Post.findById(req.params.id,function(err,post){
-    //.id automatically converts the object id to String
-    if(post.user==req.user.id){
-        post.remove();
-    //we just done here err check as we don't want further the deleted comments in DB
-        Comment.deleteMany({post:req.params.id},function(err){
+module.exports.destroy= async function(req,res){
+    try{
+        let post = await Post.findById(req.params.id);
+
+        if(post.user==req.user.id){
+            post.remove();
+        //we just done here err check as we don't want further the deleted comments in DB
+          await  Comment.deleteMany({post:req.params.id});
+          return res.redirect('back');
+        }else {
             return res.redirect('back');
-        });
-    }else{
-        return res.redirect('back');
+        }
+    }catch(err){
+        console.log('Eror',err);
+        return;
     }
-  });
+    
 }
