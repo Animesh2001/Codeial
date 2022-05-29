cfcad01896e3a4d798935fcf44403633db6886ae
Animesh Jaiswal <animeshj786@gmail.com>
2022-05-29 16:06:48 +0530

    Converting to Async Await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,40 +2,30 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home=function(req,res){
-        //  return res.end('<h1>Express is up for Codeial</h1>');
-        //  console.log(req.cookies);
-        //  res.cookie('user_id',170)
-      
-
-        //this post.find({}) query will return all the post from the database
-        // Post.find({},function(err,posts){
-        //         return res.render('home',{
-        //                 title:"Codeial | Home",
-        //                 posts:posts
-        //         });
-        // });
-
-
-        //populate the user of each post
-        Post.find({})
-        .populate('user')
-        .populate({
-                path:'comments',
-                populate:{
-                        path: 'user'
+module.exports.home= async function(req,res){
+        try{
+                //populate the user of each post
+                let posts =   await Post.find({})
+                .populate('user')
+                .populate({
+                 path:'comments',
+                 populate:{
+                   path: 'user'
                 }
-        })
-        .exec(function(err,posts){
-                User.find({},function(err,users){
-                        return res.render('home',{
-                                title:"Codeial | Home",
-                                posts:posts,
-                                all_users:users
-                        });
                 });
                 
-        })
+                let users = await User.find({});
+
+                return res.render('home',{
+                        title:"Codeial | Home",
+                        posts:posts,
+                        all_users:users
+                });
+        }catch(err){
+                console.log('Error',err);
+                return;
+        }
+      
 }
 
 //module.exports.actionName=function(req,res){}
